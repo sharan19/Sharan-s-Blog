9d38dc5ae444181620664c6d5f6f7c952af99b25
sharan19 <sharan19@users.noreply.github.com>
2020-03-28 14:32:30 +0000

    deploy: a605103e1ede293c21b180f8e52bfda57b9000bb

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://sharan19.github.io/about/",
     "title": "About Me",
-    "body": "I am machine learning engineer  Completed under graduate in mechanical engineering in 2013.  Working in my current organisation from 2018 as sales engineer.  Completed PGP in machine learning from Great Learning.  Introducing sales forecasting to my organisation.  Majorly worked in client facing roles. You can reach me through twitter or linkedin links below. "
+    "body": "Few things about me:  Completed under graduate in mechanical engineering in 2013.  Working in my current organisation from 2018 as sales engineer.  Completed PGP in machine learning from Great Learning.  Introducing sales forecasting to my organisation.  Majorly worked in client facing roles. You can reach me through twitter or linkedin links given below. "
     }, {
     "id": 2,
     "url": "https://sharan19.github.io/categories/",
